3d468373682d0863a76f438a02159be6acfc9fef
Bryan Ashley <bryan.ashley@bashley.net>
2015-01-23 17:01:16 -0800

    Updates map.js to pass along the options hash to addMarker
    This PR depends on my PR to map-canvas, found [here](https://github.com/yaronn/map-canvas/pull/1)

--- widget/map.js
+++ widget/map.js
@@ -41,12 +41,12 @@ Map.prototype.__proto__ = Canvas.prototype;
 
 Map.prototype.type = 'map';
 
-Map.prototype.addMarker = function(coord, markerType) {
+Map.prototype.addMarker = function(coord, markerType, options) {
    if (!this.innerMap) {
      throw "error: canvas context does not exist. addMArker() for maps must be called after the map has been added to the screen via screen.append()"
    }
 
-   this.innerMap.addMarker(coord, markerType)   
+   this.innerMap.addMarker(coord, markerType, options)   
 }
 
 Map.prototype.clearMarkers = function() {
