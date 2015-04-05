0e9c1df1236eac22ebec57b30bb77cf364f8863d
Yaron Naveh <yaronn01@gmail.com>
2015-04-06 01:20:39 +0300

    add multi line to dashboard

--- examples/dashboard.js
+++ examples/dashboard.js
@@ -155,9 +155,17 @@ setInterval(function() {
 //set line charts dummy data
 
 var transactionsData = {
-   title: 'server 1',
+   title: 'US',
+   style: {line: 'red'},
    x: ['00:00', '00:05', '00:10', '00:15', '00:20', '00:30', '00:40', '00:50', '01:00', '01:10', '01:20', '01:30', '01:40', '01:50', '02:00', '02:10', '02:20', '02:30', '02:40', '02:50', '03:00', '03:10', '03:20', '03:30', '03:40', '03:50', '04:00', '04:10', '04:20', '04:30'],
-   y: [0, 10, 40, 45, 45, 50, 55, 70, 65, 58, 50, 55, 60, 65, 70, 80, 70, 50, 40, 50, 60, 70, 82, 88, 89, 89, 89, 80, 72, 70]
+   y: [0, 20, 40, 45, 45, 50, 55, 70, 65, 58, 50, 55, 60, 65, 70, 80, 70, 50, 40, 50, 60, 70, 82, 88, 89, 89, 89, 80, 72, 70]
+}
+
+var transactionsData1 = {
+   title: 'Europe',
+   style: {line: 'yellow'},
+   x: ['00:00', '00:05', '00:10', '00:15', '00:20', '00:30', '00:40', '00:50', '01:00', '01:10', '01:20', '01:30', '01:40', '01:50', '02:00', '02:10', '02:20', '02:30', '02:40', '02:50', '03:00', '03:10', '03:20', '03:30', '03:40', '03:50', '04:00', '04:10', '04:20', '04:30'],
+   y: [0, 5, 5, 10, 10, 15, 20, 30, 25, 30, 30, 20, 20, 30, 30, 20, 15, 15, 19, 25, 30, 25, 25, 20, 25, 30, 35, 35, 30, 30]
 }
 
 var errorsData = {
@@ -171,31 +179,34 @@ var latencyData = {
    y: [5, 1, 7, 5]
 }
 
-setLineData(transactionsData, transactionsLine)
-setLineData(errorsData, errorsLine)
-setLineData(latencyData, latencyLine)
+setLineData([transactionsData, transactionsData1], transactionsLine)
+setLineData([errorsData], errorsLine)
+setLineData([latencyData], latencyLine)
 
 setInterval(function() {
-   setLineData(transactionsData, transactionsLine)
+   setLineData([transactionsData, transactionsData1], transactionsLine)
    screen.render()
 }, 500)
 
 setInterval(function() {   
-   setLineData(errorsData, errorsLine)
+   setLineData([errorsData], errorsLine)
    screen.render()
 }, 1500)
 
 setInterval(function() {   
-   setLineData(latencyData, latencyLine)
+   setLineData([latencyData], latencyLine)
    screen.render()
 }, 5000)
 
 function setLineData(mockData, line) {
-  var last = mockData.y[mockData.y.length-1]
-  mockData.y.shift()
-  var num = Math.max(last + Math.round(Math.random()*10) - 5, 10)    
-  mockData.y.push(num)     
-  line.setData([mockData])
+  for (var i=0; i<mockData.length; i++) {
+    var last = mockData[i].y[mockData[i].y.length-1]
+    mockData[i].y.shift()
+    var num = Math.max(last + Math.round(Math.random()*10) - 5, 10)    
+    mockData[i].y.push(num)  
+  }
+  
+  line.setData(mockData)
 }
 
 
