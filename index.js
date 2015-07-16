a0e99c0f717b086162b9023d0b271cc4418dc850
Yaron Naveh <yaronn01@gmail.com>
2015-07-16 00:02:55 +0000

    error function

--- index.js
+++ index.js
@@ -22,3 +22,4 @@ exports.line = require('./lib/widget/charts/line')
 exports.OutputBuffer = require('./lib/server-utils').OutputBuffer
 exports.InputBuffer = require('./lib/server-utils').InputBuffer
 exports.createScreen = require('./lib/server-utils').createScreen
+exports.serverError = require('./lib/server-utils').serverError
